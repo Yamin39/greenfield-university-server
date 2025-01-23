e49bb2188632f85378a7de99ecc2038327afa6f2
mdarmankhan6252 <mdarmankhan6252@gmail.com>
2025-01-23 23:34:08 +0600

    I have created an api

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6fu63x8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -82,6 +82,13 @@ async function run() {
       const result = await announcementsCollection.find().toArray();
       res.send(result);
     });
+
+    app.get('/announcement/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await announcementsCollection.findOne(query);
+      res.send(result)
+    })
 
     // faqs related apis
 
